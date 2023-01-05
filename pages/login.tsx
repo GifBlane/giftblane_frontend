7fb528f20e3bd013471f4c057d9d8c476820e642
NevercomeG <luisjavier2012@gmail.com>
2023-01-05 02:44:37 -0400

    Login Updated

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,15 +2,21 @@ import axios from "axios";
 import { NextPage } from "next";
 import Head from "next/head";
 import Router from "next/router";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "reactjs-popup/dist/index.css";
 import { Waves } from "../components/svg/Waves";
+import { useRouter } from "next/router";
 
 const Login: NextPage = () => {
-  const [submitting, setSubmitting] = useState(false);
+  const [error, setError] = useState("");
   const [email, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -18,7 +24,7 @@ const Login: NextPage = () => {
     // Verificar que la contraseña cumpla con los criterios de seguridad
     const passwordValidator =
       /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()/=+?[\]{}~-])[A-Za-z\d!@#$%^&*()/=+?[\]{}~-]{8,}$/;
-    const isPasswordValid = passwordValidator.test(password);
+    const isPasswordValid = passwordValidator.test(credentials.password);
     if (!isPasswordValid) {
       setError(
         "La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un carácter numérico y un carácter especial y debe tener al menos 8 caracteres"
@@ -27,17 +33,20 @@ const Login: NextPage = () => {
     }
     try {
       // Enviar una solicitud a la ruta de inicio de sesión en el backend
-      const response = await axios.post("http://localhost:3005/auth/login", {
-        email,
-        password,
+      const response = await axios.post("http://localhost:3002/auth/login", {
+        email: credentials.email,
+        password: credentials.password,
       });
       // Si el inicio de sesión es exitoso, almacene el token en una cookie
-      document.cookie = `token=${response.data.token}`;
+
+      document.cookie = `token=${response.data.body.token}`;
       // Redirigir a la ruta protegida
       Router.push("/dashboard");
     } catch (err: any) {
       // Obtener el estado de error y el mensaje
-      const status = err.response.status;
+
+      // Obtener el estado de error y el mensaje
+      const status = err.response.code;
       // const message = err.response.data.message;
 
       // Establecer el mensaje de error apropiado según el estado del error
@@ -62,7 +71,7 @@ const Login: NextPage = () => {
           errorMessage = "An error occurred";
           break;
       }
-
+      setError(err.response.data.message);
       setError(errorMessage);
       // Resetear el formulario de inicio de sesión
       // Deshabilitado durante desarrollo
@@ -88,7 +97,7 @@ const Login: NextPage = () => {
       >
         {/* TODO: SVG no se estira 100% */}
         <Waves className="absolute z-[0] w-full h-full pb-40" />
-        <div className="flex flex-col gap-14 items-center z-[1] pt-24">
+        <div className="flex flex-col gap-14 items-center z-[1] pt-24 ">
           <div className="text-center">
             <h1 className="font-bold text-lg">Iniciar Sesión</h1>
             <p className="text-lg">Inicia Sesión de la forma más rápida</p>
@@ -99,16 +108,20 @@ const Login: NextPage = () => {
               event.preventDefault();
               handleSubmit(event);
             }}
-            className="flex flex-col gap-2 min-h-[340px] max-w-[388px] justify-between items-center bg-[#ffffff]/80 shadow-sm rounded-[20px] py-10 px-12 "
+            className="flex flex-col gap-2 min-h-[340px] min-w-[388px] justify-between items-center bg-[#ffffff]/80 shadow-sm rounded-[20px] py-10 px-12 "
           >
             <input
               className="w-full py-2 px-5 rounded-[7px] placeholder:text-[#515151] text-black "
               name="Gmail"
-              value={email}
               required
-              disabled={submitting}
-              placeholder=""
-              onChange={(e) => setUsername(e.target.value)}
+              value={email}
+              placeholder="Gmail"
+              onChange={(e) =>
+                setCredentials({
+                  ...credentials,
+                  email: e.target.value,
+                })
+              }
             />
             <input
               className="w-full py-2 px-5 rounded-[7px] placeholder:text-[#515151] text-black "
@@ -116,35 +129,39 @@ const Login: NextPage = () => {
               name="password"
               value={password}
               required
-              disabled={submitting}
-              placeholder=""
+              placeholder="Contraseña"
               aria-label="Password"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) =>
+                setCredentials({
+                  ...credentials,
+                  password: e.target.value,
+                })
+              }
             />
             {/* Check box "Recordar Inicio de sesion" */}
-            <div>
+            {/* <div>
               <input type="checkbox" className="mr-2 w-3 h-3" />
               <label className="text-[#515151]">
                 Recordar inicio de sesión
               </label>
-            </div>
+            </div> */}
             {
               // Si hay un error, muestre el mensaje de error
               error && <p className="text-red-500">{error}</p>
             }
             <button
-              className="bg-gradient-to-b w-full from-[#DD527C] to-[#EE634C] disabled:opacity-50 rounded-[7px] text-white py-2 px-4"
+              className="bg-gradient-to-b w-full from-[#91BA4D] to-[#91BA4D] disabled:opacity-50 rounded-[7px] text-white py-2 px-4"
               type="submit"
-              disabled={submitting || !email || !password}
+              disabled={!credentials}
             >
               Iniciar Sesión
             </button>
 
             <p className="text-[#181818]">
-              ¿Olvidaste tu contraseña?{" "}
+              {/* {" "}
               <a href="" className="font-bold">
                 Click Aqui
-              </a>
+              </a> */}
             </p>
           </form>
         </div>
