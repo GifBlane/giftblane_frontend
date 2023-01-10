4d1ecb58cce78507b116d3219bd83e7be171afec
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2023-01-09 20:22:43 -0500

    use endpoint to map user data using axios

--- pages/table.tsx
+++ pages/table.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-import { usersData } from "../models/usersData";
+import axios from "axios";
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { IUser } from "../models/usersData";
 
 const TablePage: NextPage = () => {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    axios("http://localhost:3001/users").then((res) => {
+      setUsers(res.data.body.data);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Listado de usuarios</h1>
@@ -30,27 +39,30 @@ const TablePage: NextPage = () => {
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
-            {usersData.map((user, i) => (
-              <tr key={i} className="bg-white hover:bg-gray-100 ">
-                <td className="p-4 py-7 w-4">
-                  <div className="flex items-center">
-                    <input
-                      id="checkbox-table-1"
-                      type="checkbox"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label htmlFor="checkbox-table-1" className="sr-only">
-                      checkbox
-                    </label>
-                  </div>
-                </td>
-                <td className="text-left">{user.fullName}</td>
-                <td className="text-left">{user.commerce}</td>
-                <td className="text-left">{user.role}</td>
-                <td className="text-left text-[#1672EC]">{user.type}</td>
-                <td className="text-left">{user.email}</td>
-              </tr>
-            ))}
+            {users &&
+              users.map((user, i) => (
+                <tr key={i} className="bg-white hover:bg-gray-100 ">
+                  <td className="p-4 py-7 w-4">
+                    <div className="flex items-center">
+                      <input
+                        id="checkbox-table-1"
+                        type="checkbox"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                      <label htmlFor="checkbox-table-1" className="sr-only">
+                        checkbox
+                      </label>
+                    </div>
+                  </td>
+                  <td className="text-left">
+                    {user.name} {user.lastname !== null && user.lastname}
+                  </td>
+                  <td className="text-left">-</td>
+                  <td className="text-left">-</td>
+                  <td className="text-left text-[#1672EC]">{user.type_id}</td>
+                  <td className="text-left">{user.email}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
