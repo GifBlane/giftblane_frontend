9b7e118b790acfe8ec1145a7f07ff528ef2eba64
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2023-01-09 20:22:15 -0500

    change interface user

--- models/usersData.ts
+++ models/usersData.ts
@@ -1,38 +1,9 @@
-interface User {
-  fullName: string;
-  commerce: string;
-  email: string;
-  type: string;
-  role: string;
+export interface IUser {
+  id: number;
+  name: string;
+  lastname: null | string;
+  email: null | string;
+  type_id: number | null;
+  createdAt: Date;
+  updatedAt: Date;
 }
-
-export const usersData: User[] = [
-  {
-    fullName: "Pedro Jimenez",
-    commerce: "Falabella",
-    email: "pedro@falabella.com",
-    type: "Nemo",
-    role: "Administrador",
-  },
-  {
-    fullName: "Andrea Lopez",
-    commerce: "Falabella",
-    email: "andrea@falabella.com",
-    type: "Nemo",
-    role: "Vendedor",
-  },
-  {
-    fullName: "Diomedes Diaz",
-    commerce: "Koaj",
-    email: "dias@gmail.com",
-    type: "Nemo",
-    role: "Administrador",
-  },
-  {
-    fullName: "Aria Stark",
-    commerce: "Winter",
-    email: "sinrostro@bravos.com",
-    type: "Nemo",
-    role: "Administrador",
-  },
-];
